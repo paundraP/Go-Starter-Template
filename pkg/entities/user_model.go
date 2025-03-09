ed8d28ad0db56ed5da1ab4b0d3d50522a012ef79
paundraP <paundrapujodarmawan@gmail.com>
2025-03-09 14:21:26 +0700

    feat: add user model

--- pkg/entities/user_model.go
+++ pkg/entities/user_model.go
@@ -5,15 +5,15 @@ import (
 )
 
 type User struct {
-	ID        uuid.UUID `gorm:"type:uuid;primary_key;default:uuid_generate_v4()" json:"id"`
-	Name      string    `json:"name"`
-	Username  string    `json:"username"`
-	Email     string    `json:"email"`
-	Password  string    `json:"password"`
-	Subscribe bool      `gorm:"default:false" json:"subscribe"`
-	Contact   string    `json:"contact"`
-	Role      string    `json:"role"`
-	Verified  bool      `gorm:"default:false" json:"verified"`
-
+	ID             uuid.UUID `gorm:"type:uuid;primary_key;default:uuid_generate_v4()" json:"id"`
+	Name           string    `json:"name"`
+	Password       string    `json:"password"`
+	Email          string    `json:"email"`
+	About          string    `json:"about"`
+	Address        string    `json:"address"`
+	CurrentTitle   string    `json:"current_title"`
+	ProfilePicture string    `json:"profile_picture"`
+	Headline       string    `json:"headline"`
+	IsPremium      bool      `json:"is_premium"`
 	Timestamp
 }
