061963f0bf349d56e2dc75b542ca5c3872d287b0
paundraP <paundrapujodarmawan@gmail.com>
2025-03-09 23:06:14 +0700

    feat(database): add migration models

--- cmd/database/migrate/migrate.go
+++ cmd/database/migrate/migrate.go
@@ -1,7 +1,7 @@
 package migration
 
 import (
-	entities2 "Go-Starter-Template/entities"
+	"Go-Starter-Template/entities"
 	"fmt"
 	"log"
 
@@ -10,12 +10,56 @@ import (
 
 func Migrate(db *gorm.DB) error {
 	db.Exec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";")
-	if err := db.AutoMigrate(&entities2.User{}); err != nil {
+	if err := db.AutoMigrate(&entities.User{}); err != nil {
 		log.Fatalf("Error migrating user database: %v", err)
 		return err
 	}
-	if err := db.AutoMigrate(&entities2.Transaction{}); err != nil {
+	if err := db.AutoMigrate(&entities.Transaction{}); err != nil {
 		log.Fatalf("Error migrating user database: %v", err)
+		return err
+	}
+	if err := db.AutoMigrate(&entities.Companies{}); err != nil {
+		log.Fatalf("Error migrating companies database: %v", err)
+		return err
+	}
+	if err := db.AutoMigrate(&entities.JobApplication{}); err != nil {
+		log.Fatalf("Error migrating job application database: %v", err)
+		return err
+	}
+	if err := db.AutoMigrate(entities.Job{}); err != nil {
+		log.Fatalf("Error migrating job database: %v", err)
+		return err
+	}
+	if err := db.AutoMigrate(&entities.JobSkill{}); err != nil {
+		log.Fatalf("Error migrating job skill database: %v", err)
+		return err
+	}
+	if err := db.AutoMigrate(&entities.Post{}); err != nil {
+		log.Fatalf("Error migrating post database: %v", err)
+		return err
+	}
+	if err := db.AutoMigrate(&entities.Skill{}); err != nil {
+		log.Fatalf("Error migrating skill database: %v", err)
+		return err
+	}
+	if err := db.AutoMigrate(&entities.UserConnection{}); err != nil {
+		log.Fatalf("Error migrating user connection database: %v", err)
+		return err
+	}
+	if err := db.AutoMigrate(&entities.UserEducation{}); err != nil {
+		log.Fatalf("Error migrating user education database: %v", err)
+		return err
+	}
+	if err := db.AutoMigrate(&entities.UserExperience{}); err != nil {
+		log.Fatalf("Error migrating user experience database: %v", err)
+		return err
+	}
+	if err := db.AutoMigrate(&entities.UserSkill{}); err != nil {
+		log.Fatalf("Error migrating user skill database: %v", err)
+		return err
+	}
+	if err := db.AutoMigrate(&entities.Views{}); err != nil {
+		log.Fatalf("Error migrating views database: %v", err)
 		return err
 	}
 	fmt.Println("Database migration complete")
